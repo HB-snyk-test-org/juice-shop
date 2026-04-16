14f28d94b98cf07c05fa79f2d7775d693a9bd1f0
Hari Bodapati <hari.bodapati@snyk.io>
2026-04-16 10:31:08 +0100

    Add vulnerable code to test the scanner
    (cherry picked from commit 17e5cec7775ea16d88fd64372703b39a72fc09b5)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent {
       .then(response => response.json())
       .then(data => {
         // Directly injecting data into the DOM
-        document.getElementById('business-name').textContent = data.businessName;
+        document.getElementById('business-name').innerHTML = data.businessName;
       })
       .catch(error => console.error('Error fetching data:', error));
   }
